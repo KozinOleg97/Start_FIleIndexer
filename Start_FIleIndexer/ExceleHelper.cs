7ade732f1e08d2c9d1d5b9485e2717e4ba6928ee
Oleg Kosin <olegkosin@rambler.ru>
2019-10-05 13:06:32 +0300

    1

---
+++ Start_FIleIndexer/ExceleHelper.cs
@@ -0,0 +1,43 @@
+﻿using ExcelLibrary.SpreadSheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start_FIleIndexer
+{
+    class ExceleHelper
+    {
+        private string path;
+
+        private const string ExcelFileName = "Report";
+        private const string pathToExlFile = "Report\\";
+
+
+        public ExceleHelper(string path)
+        {
+            this.path = path;
+
+            Workbook workbook = new Workbook();
+            Worksheet worksheet = new Worksheet("Report");
+
+            // create 100 empty cells for Excel 2010>
+            for (int i = 0; i < 100; i++)
+                worksheet.Cells[i, 0] = new Cell("");
+
+            workbook.Worksheets.Add(worksheet);
+            workbook.Save(path + pathToExlFile + ExcelFileName + ".xls");
+        }
+
+        public Workbook LoadFromFile()
+        {
+
+            return Workbook.Load("path");
+        }
+
+
+
+
+    }
+}
--- Start_FIleIndexer/Program.cs
+++ Start_FIleIndexer/Program.cs
@@ -10,6 +10,48 @@ namespace Start_FIleIndexer
     {
         static void Main(string[] args)
         {
+
+            String path = "E:\\Programming\\Start Testing Folder\\";
+
+            //Console.WriteLine("Path:");
+            //path = Console.ReadLine();            
+
+            //creating fileName
+            String newExcelFileName = "Report";// + DateTime.Now.ToString("yyyy-MM-dd (h mm ss)tt");
+
+            ExceleHelper excele = new ExceleHelper(path);
+
+
+            Workbook workbook = excele.LoadFromFile();
+
+            //initialization & new doc
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+
+            //write to cells
+            FileInfo[] allFiles = dir.GetFiles("*.doc");
+
+            for (int cnt = 0; cnt < allFiles.Length; cnt++)
+            {
+
+                worksheet.Cells[cnt, 0] = new Cell(allFiles[cnt].Name);
+                worksheet.Cells[cnt, 1] = new Cell(cnt);
+                File.Move(allFiles[cnt].FullName, path + cnt + "_" +
+                    Path.GetFileNameWithoutExtension(allFiles[cnt].Name) + ".doc");
+
+            }
+
+
+            // System.IO.File.Move("oldfilename", "newfilename");
+
+            //save
+            workbook.Worksheets.Add(worksheet);
+            workbook.Save(path + newExcelFileName + ".xls");
+
+
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
         }
     }
 }
